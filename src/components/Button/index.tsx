c6b7072b9ac010faed611b4d81e78d0c273c8c44
mojqva <majanka@i.ua>
2021-05-19 22:19:27 +0300

    Очень понравился хук

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -12,16 +12,16 @@ interface ButtonProps {
     size: sizeType;
 }
 
-const Button: React.FC<ButtonProps> = ({ children, onClick , ...props}) => {
+const Button: React.FC<ButtonProps> = ({ children, onClick , width, color, size}) => {
     return (
         <button 
             type="button" 
             className={s.root} 
             onClick={onClick} 
             style={{
-                width: (props.width === 'max') ? '100%' : '231px',
-                background: (props.color === 'yellow') ? '#F5DB13' : '#73D677',
-                height: (props.size === 'small') ? '16px' : '66px',
+                width: (width === 'max') ? '100%' : '231px',
+                background: (color === 'yellow') ? '#F5DB13' : '#73D677',
+                height: (size === 'small') ? '16px' : '66px',
             }}
         >
             {children}
