c3e3494d219a459057dff882632c71247c9ef948
mojqva <majanka@i.ua>
2021-05-21 00:07:21 +0300

    Проработал недочёты по прошлому дз

--- src/pages/Pokedex/index.tsx
+++ src/pages/Pokedex/index.tsx
@@ -1,35 +1,23 @@
-import React from 'react'
-import Header from '../../components/Header'
-import PokemonCard from '../../components/PokemonCard'
-import { pokemons } from '../../pokemons'
+import React from 'react';
+import Header from '../../components/Header';
+import PokemonCard from '../../components/PokemonCard';
+import { pokemons } from '../../pokemons';
 
-import s from './Pokedex.module.scss'
+import s from './Pokedex.module.scss';
 
 const PokedexPage = () => {
-
     return (
         <>
-            <Header/>
+            <Header />
             <div>
                 <div className={s.pokemons}>
-                    {
-                        pokemons.map(({name, stats, types, img}, index) => {
-                            return (
-                            <PokemonCard
-                                key={index}
-                                name={name}
-                                attack={stats.attack}
-                                defense={stats.defense}
-                                types={types}
-                                img={img}
-                            />
-                        )
-                        })
-                    }
+                    {pokemons.map(({ name, stats, types, img, id }) => {
+                        return <PokemonCard key={id} name={name} stats={stats} types={types} img={img} />;
+                    })}
                 </div>
             </div>
         </>
-    )
-}
+    );
+};
 
-export default PokedexPage
+export default PokedexPage;
